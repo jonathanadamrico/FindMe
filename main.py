a42655c154d65abad43f33ad813696fc1ee61f5b
Jonathan Adam Rico <51609531+jonathanadamrico@users.noreply.github.com>
2022-01-01 16:02:06 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -16,18 +16,20 @@ def main():
 
     image_src = st.file_uploader("Big picture", type='png')
     if image_src is not None:
-        img_rgb = np.array(Image.open(image_src))
+        img_rgb = Image.open(image_src)
         template_src = st.file_uploader("Object to find", type='png')
         st.image(img_rgb, caption='Big Picture', use_column_width=True)
+        st.write("***")
         
         if template_src is not None:
-            template = np.array(Image.open(template_src))
+            template = Image.open(template_src)
             
-            img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-            template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+            img_gray = cv2.cvtColor(np.array(img_rgb), cv2.COLOR_BGR2GRAY)
+            template = cv2.cvtColor(np.array(template), cv2.COLOR_BGR2GRAY)
 
             st.image(template, caption='Object to Find', use_column_width=False)
             height, width = template.shape[::]
+            st.write("***")
     
             st.write("Finding objects...")
     
@@ -38,10 +40,15 @@ def main():
             loc = np.where( result >= threshold)  
 
             for pt in zip(*loc[::-1]): 
-                cv2.rectangle(img_rgb, pt, (pt[0] + width, pt[1] + height), (0, 255, 0), 1)
-            st.image(img_rgb, caption='Objects found', use_column_width=True)
+                top_left = pt
+                bottom_right = (pt[0] + width, pt[1] + height)
+                cv2.rectangle(img_gray, top_left, bottom_right, (0, 255, 0), 1)
+                
+            st.write("***")
+            st.write("### Results")
+            st.image(img_gray, caption='Objects found', use_column_width=True)
 
-
+            st.write("### Thank you!")
     
     
 
