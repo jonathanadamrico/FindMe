cc5736ec2cee7e86a64cc72e34e3333dfa9cfe05
Jonathan Adam Rico <51609531+jonathanadamrico@users.noreply.github.com>
2022-01-01 14:41:03 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ def main():
     image_src = st.file_uploader("Big picture", type='png')
     if image_src is not None:
         img_rgb = Image.open(image_src)
-        st.image(img_rgb, caption='Uploaded Image.', use_column_width=True)
-        template_src = st.file_uploader("What to find", type='png')
+        st.image(img_rgb, caption='Big Picture', use_column_width=True)
+        template_src = st.file_uploader("Object to find", type='png')
         if template_src is not None:
             template = Image.open(template_src)
-            st.image(template, caption='Uploaded Image.', use_column_width=False)
+            st.image(template, caption='Object to Find', use_column_width=False)
     
             try:
                 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
@@ -34,6 +34,8 @@ def main():
 
             height, width = template.shape[::]
     
+            st.write("Finding objects...")
+    
             result = cv2.matchTemplate(img_gray, template, cv2.TM_SQDIFF)
             plt.imshow(result, cmap='gray')
 
@@ -41,7 +43,7 @@ def main():
 
             loc = np.where( result >= threshold)  
 
-            for pt in zip(loc[::-1]): 
+            for pt in zip(*loc[::-1]): 
                 cv2.rectangle(img_gray, pt, (pt[0] + width, pt[1] + height), (255, 0, 0), 1)
             plt.imshow(img_gray)
 
